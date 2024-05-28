b80e3017cdf6261aa9fa0a1b8265fce18fc17b12
jeanluck77 <jndocho01@gmail.com>
2024-05-27 19:08:08 -0600

    added multiplication

--- calculator.ps1
+++ calculator.ps1
@@ -1,1 +1,15 @@
-﻿function Add-Numbers([int]$a, [int]$b) {    $sum = $a + $b    return $sum}$result = Add-Numbers -a 5 -b 3Write-Host $result
+﻿function Add-Numbers([int]$a, [int]$b) {
+    $sum = $a + $b
+    return $sum
+}
+
+function Multiply-Numbers([int]$a, [int]$b) {
+    $product = $a * $b
+    return $product
+}
+
+$resultOfSum = Add-Numbers -a 5 -b 3
+Write-Host $resultOfSum
+
+$resultOfMul = Multiply-Numbers -a 3 -b 3
+Write-Host $resultOfMul
