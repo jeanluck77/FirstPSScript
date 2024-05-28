c1103aba206241d4f44cfec6329d9825f4c7dd69
CCurtis72 <chris.curtis@shaw.ca>
2024-05-27 19:25:33 -0600

    Added Divide Function

--- calculator.ps1
+++ calculator.ps1
@@ -13,8 +13,21 @@ function SubStract-Numbers([int]$a, [int]$b) {
     return $difference
 }
 
+
+function Divide-Numbers([int]$a, [int]$b) {
+    $dividend = $a / $b
+    return $dividend
+}
+
 $resultOfSum = Add-Numbers -a 5 -b 3
 Write-Host $resultOfSum
 
 $resultOfMul = Multiply-Numbers -a 3 -b 3
 Write-Host $resultOfMul
+
+$resultOfSub = SubStract-Numbers -a 3 -b 3
+Write-Host $resultOfSub
+
+$resultOfDiv = Divide-Numbers = -a 3 -b 3
+Write-Host $resultOfDiv
+
